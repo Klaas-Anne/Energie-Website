d3b7010ab99c2550706f37e3472507d97f27cf3d
Johan Kladder <j.kladder@st.hanze.nl>
2017-10-13 11:06:45 +0200

    + On change code

--- ui/src/Register.js
+++ ui/src/Register.js
@@ -4,35 +4,75 @@ class Register extends Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            number: 1,
+            email: '',
+            street: '',
+            zipcode: '',
+            place: ''
+        };
+
+        console.log(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleSubmit() {
+
+    }
+
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        console.log(name);
+        console.log(value);
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
+
         return (
-            React.createElement('form', {className: 'ContactForm'},
+            React.createElement('form', {className: 'ContactForm', onSubmit: this.handleSubmit},
                 React.createElement('input', {
                     type: 'number',
+                    name: 'number',
                     placeholder: 'Inwoners',
-                    //value: this.props.value.name,
+                    value: this.state.number,
+                    onChange: this.handleInputChange
                 }),
                 React.createElement('input', {
                     type: 'email',
+                    name: 'email',
                     placeholder: 'Email',
-                    //value: this.props.value.email,
+                    value: this.state.email,
+                    onChange: this.handleInputChange
                 }),
                 React.createElement('input', {
                     type: 'text',
+                    name: 'street',
                     placeholder: 'Street',
-                    //value: this.props.value.description,
+                    value: this.state.street,
+                    onChange: this.handleInputChange
                 }),
                 React.createElement('input', {
                     type: 'text',
+                    name: 'zipcode',
                     placeholder: 'Zipcode',
-                    //value: this.props.value.description,
+                    value: this.state.zipcode,
+                    onChange: this.handleInputChange
                 }),
                 React.createElement('input', {
                     type: 'text',
+                    name: 'place',
                     placeholder: 'Place',
-                    //value: this.props.value.description,
+                    value: this.state.place,
+                    onChange: this.handleInputChange
                 }),
                 React.createElement('button', {type: 'submit'}, "Register")
             )
